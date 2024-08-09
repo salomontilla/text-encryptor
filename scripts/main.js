79b782bd4a3adbdbc10f078d48d0230188e0a9ff
salo <salomontilla@outlook.com>
2024-08-09 13:54:18 -0500

    removed animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,4 @@
 function hideInitialDiv() {
-    const div = document.getElementById("initial_state");
-    div.style.animation = "fadeOut 0.5s ease forwards";
-    
-    
-    document.getElementById("final_state").style.display = "flex"
-    
-
+   document.getElementById("initial_state").style.display = "none";
+   document.getElementById("final_state").style.display = "flex";
 }
