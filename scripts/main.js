93e09064d0752b992d7767b7960448311879483e
salo <salomontilla@outlook.com>
2024-08-09 16:33:50 -0500

    organizing finalState

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
 function hideInitialDiv() {
    document.getElementById("initial_state").style.display = "none";
    document.getElementById("final_state").style.display = "flex";
-}
+}//recordar cambiar el main a 100%
