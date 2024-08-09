a2f0905529810f16fe227916f1317706d4e45ef7
salo <salomontilla@outlook.com>
2024-08-09 16:58:13 -0500

    changin main to make the page expandible

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
+const mediaQuery = window.matchMedia("(max-width: 768px)");
 function hideInitialDiv() {
    document.getElementById("initial_state").style.display = "none";
    document.getElementById("final_state").style.display = "flex";
-}//recordar cambiar el main a 100%
+}
+
+    
