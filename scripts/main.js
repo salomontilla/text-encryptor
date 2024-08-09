cba29753cc1964355c8db388d7347240b163a86d
salo <salomontilla@outlook.com>
2024-08-09 13:25:10 -0500

    adding fadeout animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
 function hideInitialDiv() {
-    document.getElementById("initial_state").style.display = "none";
+    const div = document.getElementById("initial_state");
+    div.style.animation = "fadeOut 0.5s ease forwards";
+    
+    
+    document.getElementById("final_state").style.display = "flex"
+    
+
 }
