9d4270e8dcc21757ea51034b24529d3727996cdb
salo <salomontilla@outlook.com>
2024-08-10 23:17:03 -0500

    function encrypt

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,18 @@
 
 function encrypt() {
-   let textContent = document.getElementById('textArea').value;
-   if(textContent!= ''){
+   let textarea = document.getElementById('textArea');    
+   let contenido = textarea.value;
+   let textElement = document.getElementById('textConverted');
+
+   if(contenido != ''){
+      textElement.textContent = contenido
+      
+      .replace('e', 'enter')
+      .replace('i', 'imes')
+      .replace('o', 'ober')
+      .replace('u', 'ufat')
+      .replace('a', 'ai');
+       
       document.getElementById("initial_state").style.display = "none";
       document.getElementById("final_state").style.display = "flex";
    }
