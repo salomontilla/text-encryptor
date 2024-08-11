3d968e24451d890a66885b74c8b2e97f7c708e79
salo <salomontilla@outlook.com>
2024-08-11 12:31:46 -0500

    create function copy

--- scripts/main.js
+++ scripts/main.js
@@ -1,49 +1,50 @@
 
 function encrypt() {
-   let contenido = document.getElementById('textArea').value;    
+   let contenido = document.getElementById('textArea').value;
    let textElement = document.getElementById('textConverted');
 
-   if(contenido != ''){
+   if (contenido != '') {
       textElement.textContent = contenido
-      
-      .replace(/e/g, 'enter')
-      .replace(/i/g, 'imes')
-      .replace(/o/g, 'ober')
-      .replace(/u/g, 'ufat')
-      .replace(/a/g, 'ai');
-       
-      document.getElementById("initial_state").style.display = "none";
-      document.getElementById("final_state").style.display = "flex";
-   }else{
-      document.getElementById("initial_state").style.display = "flex";
-      document.getElementById("final_state").style.display = "none";
-   }
-   
-   
-}
-function decrypt(){
-   let contenido = document.getElementById('textArea').value;    
-   let textElement = document.getElementById('textConverted');
 
-   if(contenido != ''){
-      textElement.textContent = contenido
-      
-      .replace(/enter/g, 'e')
-      .replace(/imes/g, 'i')
-      .replace(/ober/g, 'o')
-      .replace(/ufat/g, 'u')
-      .replace(/ai/g, 'a');
+         .replace(/e/g, 'enter')
+         .replace(/i/g, 'imes')
+         .replace(/o/g, 'ober')
+         .replace(/u/g, 'ufat')
+         .replace(/a/g, 'ai');
 
       document.getElementById("initial_state").style.display = "none";
       document.getElementById("final_state").style.display = "flex";
-   
-   }else{
+   } else {
       document.getElementById("initial_state").style.display = "flex";
       document.getElementById("final_state").style.display = "none";
    }
-       
-      
+
 
 }
+function decrypt() {
+   let contenido = document.getElementById('textArea').value;
+   let textElement = document.getElementById('textConverted');
 
-    
+   if (contenido != '') {
+      textElement.textContent = contenido
+
+         .replace(/enter/g, 'e')
+         .replace(/imes/g, 'i')
+         .replace(/ober/g, 'o')
+         .replace(/ufat/g, 'u')
+         .replace(/ai/g, 'a');
+
+      document.getElementById("initial_state").style.display = "none";
+      document.getElementById("final_state").style.display = "flex";
+
+   } else {
+      document.getElementById("initial_state").style.display = "flex";
+      document.getElementById("final_state").style.display = "none";
+   }
+}
+
+
+async function copy() {
+   let text = document.getElementById("textConverted").textContent;
+   navigator.clipboard.writeText(text);
+}
